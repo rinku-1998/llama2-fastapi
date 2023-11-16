60a34ff5aa20898af9e4f4490e4cd314b05c5377
Rinku <shark4rkcoding@gmail.com>
2023-11-16 14:48:24 +0800

    feat: 加入日誌功能

--- api/__init__.py
+++ api/__init__.py
@@ -1,7 +1,11 @@
+import logging
+import sys
 from api.exception_handler import attach_exception_handlers
 from api.route import router
+from api.logging_handler import format_record, InterceptHandler
 from fastapi import FastAPI
 from llama_cpp import Llama
+from loguru import logger
 
 
 def create_app() -> FastAPI:
@@ -13,10 +17,29 @@ def create_app() -> FastAPI:
     llm = Llama(model_path=model_path)
     app.state.llm = llm
 
-    # 3. 註冊路由
+    # 3. 設定日誌格式
+    # 格式
+    logger.configure(handlers=[{
+        "sink": sys.stdout,
+        "level": logging.DEBUG,
+        "format": format_record
+    }])
+
+    # Uvicorn
+    logging.getLogger().handlers = [InterceptHandler()]
+    logging.getLogger("uvicorn.access").handlers = [InterceptHandler()]
+
+    # 新增儲存位置
+    logger.add("logs/run.log",
+               rotation="500MB",
+               encoding="utf-8",
+               enqueue=True,
+               retention="15 days")
+    
+    # 4. 註冊路由
     app.include_router(router, prefix='/api')
 
-    # 4. 註冊全局例外
+    # 5. 註冊全局例外
     app = attach_exception_handlers(app)
 
     return app
